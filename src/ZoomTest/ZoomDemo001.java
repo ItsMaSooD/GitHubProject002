462213ea0158be82e7f1df322d9bfb9b4c55f23e
masoo <masoo@192.168.1.170>
2023-07-29 11:28:16 -0500

    Commit Push n Stash

--- src/ZoomTest/ZoomDemo001.java
+++ src/ZoomTest/ZoomDemo001.java
@@ -4,5 +4,9 @@ public class ZoomDemo001 {
 	public static void main(String [] args) {
 		System.out.println("Demo 1...");
 	}
+	
+	public static void info() {
+		System.out.println("Stash...");
+	}
 
 }
